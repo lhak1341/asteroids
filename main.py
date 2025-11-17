2eb7985953f99460852ea4e7588d309dc1b90445
Khoa Le <lhak1341@gmail.com>
2025-11-17 12:51:16 +0700

    4.3 Shoot-able

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import pygame, sys
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
 from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED)
+    pygame.display.set_caption("Asteroids")
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -17,25 +19,31 @@ def main():
 
     asteroids = pygame.sprite.Group()
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
+    AsteroidField.containers = (updatable,)
+
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroidfield = AsteroidField()
 
-    while True:
+    running = True
+    while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(FPS) / 1000.0
         updatable.update(dt)
 
         for a in asteroids:
             if a.collides_with(player):
                 log_event("player_hit")
                 print("Game over!")
-                sys.exit()
+                # sys.exit()
+                running = False
+                break
 
         screen.fill("black")
         for d in drawable:
@@ -45,6 +53,7 @@ def main():
     # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.quit()
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,25 +1,30 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.can_shoot = True
+
+    def forward(self):
+        return pygame.Vector2(0, 1).rotate(self.rotation)
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        f = self.forward()
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        a = self.position + f * self.radius
+        b = self.position - f * self.radius - right
+        c = self.position - f * self.radius + right
         return [a, b, c]
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
+        self.rotation = (self.rotation + PLAYER_TURN_SPEED * dt) % 360
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -32,9 +37,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * -1)
+        if keys[pygame.K_SPACE] and self.can_shoot:
+            self.shoot()
+            self.can_shoot = False
+        if not keys[pygame.K_SPACE]:
+            self.can_shoot = True
 
     def move(self, dt):
-        unit_vector = pygame.Vector2(0, 1)
-        rotated_vector = unit_vector.rotate(self.rotation)
-        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
-        self.position += rotated_with_speed_vector
+        self.position += self.forward() * PLAYER_SPEED * dt
+
+    def shoot(self):
+        s = Shot(self.position.x, self.position.y)
+        s.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
